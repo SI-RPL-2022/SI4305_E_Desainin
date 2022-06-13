02aad12a49dc095a1f761dd173bf5b8a6691c1d9
rifqisetiawann <setiawanmuhammadrifqi@gmail.com>
2022-06-13 21:01:29 +0700

    Edit collection

--- resources/views/collections.blade.php
+++ resources/views/collections.blade.php
@@ -14,13 +14,24 @@
                     <h5 class="card-text text-muted">by <a
                             href="/{{ $posts[0]->user->username }}">{{ $posts[0]->user->fullname }}</a> |
                         {{ $posts[0]->created_at->diffForHumans() }}</h5>
-                    <h5 class="card-text">{{ $posts[0]->body }}</h5>
+                    <a href="/post-{{ $posts[0]->id }}" class="card-text text-decoration-none">
+                        View More
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z">
+                            </path>
+                            <path fill-rule="evenodd"
+                                d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z">
+                            </path>
+                        </svg>
+                    </a>
                 </div>
             </div>
 
             <div class="row row-cols-1 row-cols-md-3 g-4 mt-1 pt-1">
                 @foreach ($posts->skip(1) as $post)
-                @include('post.partial.loopcardpost')
+                    @include('post.partial.loopcardpost')
                 @endforeach
             </div>
         </div>
@@ -29,6 +40,6 @@
             <h2 id="title"><b>
                     <center>No portfolio posted by user.</center>
                 </b></h2>
-        </div>        
+        </div>
     @endif
 @endsection
